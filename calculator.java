d506a17616b71affed97a42cd07577702604dca9
Balaji <svbalaji03@gmail.com>
2022-09-25 15:08:48 +0530

    first commit

---
+++ calculator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class calculator
+{
+    public static void main(String[] args) {
+        int a;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number to be printed there: ");
+        a = in.nextInt();
+        System.out.println(a);
+        in.close();
+    }
+}
---
+++ triangle.java
@@ -0,0 +1,9 @@
+public class triangle 
+{
+    public static void main(String[] args) 
+    {
+          
+       System.out.print("*"); 
+    }
+    
+}
