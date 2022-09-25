44fe042c048dc58a1c39b8dc49efd2d96b7aff70
Balaji <svbalaji03@gmail.com>
2022-09-25 15:22:42 +0530

    sec commit

--- triangle.java
+++ triangle.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 public class triangle 
 {
     public static void main(String[] args) 
     {
-          
-       System.out.print("*"); 
+            int n;
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter the number of rows: ");
+            n=in.nextInt();
+            for(int i=0;i<n;i++)
+            {
+             System.out.print("*");
+            }   
+            in.close(); 
     }
-    
 }
+    
+
