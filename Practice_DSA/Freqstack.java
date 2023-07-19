2e67acb07c47589e17a7365691a8af15e1aaedde
Balaji S V <86550781+Balaji-S-V@users.noreply.github.com>
2023-07-19 12:39:53 +0530

    movedfiles

---
+++ Practice_DSA/Freqstack.java
@@ -0,0 +1,43 @@
+class Stack
+{
+    int top;
+    int arr[];
+    int capacity;
+    Stack(int x)
+    {
+        arr = new int[x];
+        capacity = x;
+        top = -1;
+    }
+    public void push()
+    {
+        if(isFull())
+            {
+                System.out.println("Stack overflow");
+            }
+            System.out.println("Inserting the Element " + value);
+            arr[++top] = value;
+    } 
+    public void pop()
+    {
+        if(isempty())
+        {
+            System.out.println("No elements in stack");
+        }
+        return arr[top--];
+    }
+    boolean isempty()
+    {
+        return top == -1
+    }
+    boolean isFull()
+    {
+        return top ==  capacity-1;
+    }
+}
+class Freqstack
+{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Practice_DSA/Minswap.java
@@ -0,0 +1,11 @@
+public class Minswap 
+{
+    public void swap(int[] arr)
+    {
+        int len = arr.length();
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
